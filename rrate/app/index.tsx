e6d3667830a27f81c8a3234b85481cbbc676107e
Anna Friesen <annafriesen@shaw.ca>
2025-06-02 16:16:38 -0700

    added customized colour scheme to assets

--- rrate/app/index.tsx
+++ rrate/app/index.tsx
@@ -1,4 +1,9 @@
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Theme } from "../assets/theme";
+
+
 
 export default function Index() {
   return (
@@ -9,7 +14,26 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', gap: 12 }}>
+        <Button
+          mode="contained"
+          buttonColor={Theme.colors.secondary}
+          onPress={() => console.log('Pressed')}
+          icon={({ size, color }) => (
+            <MaterialCommunityIcons
+              name="location-exit"
+              size={size}
+              color={color}
+              style={{ transform: [{ rotate: '180deg' }] }}
+            />
+          )}
+        >
+          Exit
+        </Button>
+        <Button icon="cog" buttonColor={Theme.colors["neutral-bttn"]} mode="contained" onPress={() => console.log('Pressed')}>
+          Settings
+        </Button>
+      </View>
+    </View >
   );
 }
---
+++ rrate/assets/theme.ts
@@ -0,0 +1,51 @@
+import {
+  MD3LightTheme as DefaultTheme
+} from 'react-native-paper';
+
+export const Theme = {
+  ...DefaultTheme,
+  colors: {
+    "primary": "rgb(39, 93, 172)",
+    "onPrimary": "rgb(255, 255, 255)",
+    "primaryContainer": "rgb(215, 226, 255)",
+    "onPrimaryContainer": "rgb(0, 27, 63)",
+    "secondary": "rgb(0, 110, 46)",
+    "onSecondary": "rgb(255, 255, 255)",
+    "secondaryContainer": "rgb(135, 251, 156)",
+    "onSecondaryContainer": "rgb(0, 33, 9)",
+    "tertiary": "rgb(187, 23, 10)",
+    "onTertiary": "rgb(255, 255, 255)",
+    "tertiaryContainer": "rgb(255, 218, 212)",
+    "onTertiaryContainer": "rgb(65, 1, 0)",
+    "error": "rgb(186, 26, 26)",
+    "onError": "rgb(255, 255, 255)",
+    "errorContainer": "rgb(255, 218, 214)",
+    "onErrorContainer": "rgb(65, 0, 2)",
+    "neutral-bttn": "rgb(142, 142, 147)",
+    "background": "rgb(253, 251, 255)",
+    "onBackground": "rgb(26, 27, 31)",
+    "surface": "rgb(253, 251, 255)",
+    "onSurface": "rgb(26, 27, 31)",
+    "surfaceVariant": "rgb(224, 226, 236)",
+    "onSurfaceVariant": "rgb(68, 71, 78)",
+    "outline": "rgb(116, 119, 127)",
+    "outlineVariant": "rgb(196, 198, 208)",
+    "shadow": "rgb(0, 0, 0)",
+    "scrim": "rgb(0, 0, 0)",
+    "inverseSurface": "rgb(47, 48, 51)",
+    "inverseOnSurface": "rgb(242, 240, 244)",
+    "inversePrimary": "rgb(171, 199, 255)",
+    "elevation": {
+      "level0": "transparent",
+      "level1": "rgb(242, 243, 251)",
+      "level2": "rgb(236, 238, 248)",
+      "level3": "rgb(230, 234, 246)",
+      "level4": "rgb(227, 232, 245)",
+      "level5": "rgb(223, 229, 243)"
+    },
+    "surfaceDisabled": "rgba(26, 27, 31, 0.12)",
+    "onSurfaceDisabled": "rgba(26, 27, 31, 0.38)",
+    "backdrop": "rgba(45, 48, 56, 0.4)"
+  }
+};
+
