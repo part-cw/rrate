2507d6d0db38ad32a695d9c681b222bf349189d3
Anna Friesen <annafriesen@shaw.ca>
2025-06-25 13:46:37 -0700

    replaced UBC logo with IGH logo

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -5,10 +5,10 @@ export default function Copyright() {
   return (
     <View style={{ margin: 50, alignItems: 'center' }}>
 
-      <Image style={{ width: 80, height: 80 }} source={require('../assets/images/ubc-logo.png')} />
+      <Image style={{ width: 200, height: 80, margin: 0 }} source={require('../assets/images/IGH-logo.png')} />
 
-      <Text style={{ fontWeight: 'bold', padding: 5 }}> RRate v 3.0 </Text>
-      <Text style={{ padding: 5 }}> University of British Columbia </Text>
+      <Text style={{ fontWeight: 'bold', padding: 5 }}>RRate v 3.0</Text>
+      <Text style={{ padding: 5 }}>Institute for Global Health</Text>
       <Text> Copyright 2018 </Text>
     </View >
   );
