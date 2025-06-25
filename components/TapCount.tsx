1b2dba93a57a79d4887280a676fc17af80be0fda
Anna Friesen <annafriesen@shaw.ca>
2025-06-25 14:23:16 -0700

    added a Tap placeholder to maintain component size

--- components/TapCount.tsx
+++ components/TapCount.tsx
@@ -14,16 +14,24 @@ export default function TapCount({ tapCount }: TapCountProps) {
         Tap Count
       </Text>
       <View style={Style.circleRow}>
-        {Array.from({ length: tapCount }).map((_, index) => (
-          <View
-            key={index}
+        {tapCount > 0 ? (
+          Array.from({ length: tapCount }).map((_, index) => (
+            <View
+              key={index}
+              style={[
+                Style.tapCircle,
+                { backgroundColor: '#4267BC' }
+              ]}
+            />
+          ))
+        )
+          : // If no taps, have a invisible placeholder circle to maintain size of component
+          (<View
             style={[
               Style.tapCircle,
-              // { backgroundColor: index < tapCount ? '#4267BC' : '#D3D3D3' }
-              { backgroundColor: '#4267BC' }
+              { backgroundColor: '#f5f6f7', borderColor: '#f5f6f7' }
             ]}
-          />
-        ))}
+          />)}
       </View>
     </View >
   )
