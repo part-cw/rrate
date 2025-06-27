c2e0e020d13fe202fb113b129b75c08e06a8dd30
Anna Friesen <annafriesen@shaw.ca>
2025-06-27 11:59:45 -0700

    removed unnecessary line  in baby animation

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -5,9 +5,9 @@ export default function Copyright() {
   return (
     <View style={{ margin: 50, alignItems: 'center' }}>
       <Image style={{ width: 120, height: 80, margin: 0 }} source={require('../assets/images/IGH-logo.png')} />
-      <Text style={{ fontWeight: 'bold', padding: 5 }}>RRate v 3.0</Text>
+      <Text style={{ fontWeight: 'bold', padding: 5 }}>RRate v 4.0</Text>
       <Text style={{ padding: 5 }}>Institute for Global Health</Text>
-      <Text> Copyright 2018 </Text>
+      <Text> Copyright 2025 </Text>
     </View >
   );
 }
