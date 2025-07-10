a236a7036b9047cbd102fa0503d49d18a39cab4c
Anna Friesen <annafriesen@shaw.ca>
2025-07-10 15:55:12 -0700

    removed jest native

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
     'node_modules/(?!(expo|@expo|expo-modules-core|react-native|@react-native|@react-navigation|expo-router)/)',
   ],
